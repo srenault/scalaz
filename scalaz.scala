7bad5945d5c165d0d05d085ebd245a8c0fb1a2e2
Sébastien RENAULT <srenault.contact@gmail.com>
2012-10-22 09:46:49 +0200

    Type class

--- scalaz.scala
+++ scalaz.scala
@@ -1,17 +1,54 @@
 import scalaz._
 import scalaz.Scalaz._
+import scala.collection.immutable.NumericRange
 
 object Scalaz {
   def main(args: Array[String]) {
     println("### Equals")
-    assert(1 === 1, true)
+    1 === 1
     //1 === "1" ;compilation error
-    assert(1.some =/= 2.some, true)
+    1.some =/= 2.some
 
     println("### Ordering")
-    assert(1 > 2.0, false)
+    (1 > 2.0) === false
     //1 gt 2.0 ;compilation error
-    assert((1.0 ?|? 2.0) === LT, true)
-    assert((1.0 max 2.0) === 2.0, true)
+    (1.0 ?|? 2.0) === LT
+    (1.0 max 2.0) == 2.0
+
+    println("### Show")
+    3.shows == "3"
+
+    println("### Enum")
+    ('a' to 'e').toList == ('a' |-> 'e')
+    ('b'.succ == 'c') == true
+
+    println("### Typeclass")
+  }
+}
+
+/**
+ * Typed constructor : How I can convert a to boolean
+ */
+trait CanTruthy[A] { self =>
+  def truthys(a: A): Boolean
+}
+
+object CanTruthy {
+  def apply[A](implicit ev: CanTruthy[A]): CanTruthy[A] = ev
+  def truthys[A](f: A => Boolean): CanTruthy[A] = new CanTruthy[A] {
+    def truthys(a: A) = f(a)
+  }
+}
+
+trait CanTruthyOps[A] {
+  def self: A
+  implicit def F: CanTruthy[A]
+  final def truthy: Boolean = F.truthys(self)
+}
+
+object ToCanIsTruthyOps {
+  implicit def toCanIsTruthyOps[A](v: A)(implicit ev: CanTruthy[A]) = new CanTruthyOps[A] {
+    def self = v
+    implicit def F: CanTruthy[A] = ev
   }
 }
